08de976a7154397028ef1fed340cff92eda6e5c5
Milan Sekulic <sekulic.milan@protonmail.com>
2025-07-01 11:51:06 +0200

    refactor: vectorise team icon

--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -7,9 +7,9 @@ useHead({
 </script>
 
 <template>
-  <div class="min-h-screen flex flex-col justify-center px-6 lg:px-10 pt-12 md:pt-20">
+  <div class="min-h-screen flex flex-col justify-center px-6 lg:px-10 pt-8 md:pt-20">
     <div class="max-w-[800px] mx-auto">
-      <img src="/images/doctors.png" alt="Team placeholder" class="h-[100px] md:w-auto mx-auto">
+      <img src="/images/doctors.svg" alt="Team placeholder" class="h-32 md:w-auto mx-auto">
       <p class="text-center text-gray-600 mt-2 text-xl">Uskoro više informacija</p>
     </div>
   </div>
